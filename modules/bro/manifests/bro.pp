28b79d87f7af06c1c9f63f646dc502630c5eb062
Matthew Morrison <morrison@simspace.com>
2018-02-19 11:02:02 -0500

    Adding bro user so bro will run as non-root

--- modules/bro/manifests/bro.pp
+++ modules/bro/manifests/bro.pp
@@ -5,11 +5,16 @@ class bro::bro inherits bro {
     notify        =>  Exec['install bro'],
     unless        =>  "test -d ${broinstall_dir}",
   }
-
   exec { 'install bro' :
     command       =>  "bash configure --with-pcap=${pfringinstall_dir} --prefix=${broinstall_dir} && make -j ${facts['processors']['count']} && make install",
     cwd           =>  "${brotmp_dir}",
     path          =>  '/usr/bin:/usr/sbin:/bin:/sbin',
     timeout       =>  '0',
+    refreshonly   =>  'true',
+  } ->
+  user { 'bro' :
+    ensure        =>  'present',
+    home          =>  "${broinstall_dir}",
+    shell         =>  '/usr/sbin/nologin',
   }
 }
