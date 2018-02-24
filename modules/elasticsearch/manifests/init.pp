4946d38c649f539011cb3a1c75124ccb75e80be7
Matthew Morrison <matthewmorrison89@gmail.com>
2018-02-24 18:35:45 -0500

    missed this file on the last commit

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -1,11 +1,11 @@
 class elasticsearch (
-    $es_clustername						= $hostname,
+    $es_clustername           = $hostname,
     $es_nodename              = $fqdn,
     $es_noderack              = 'r1',
     $es_memorylock            = 'true',
     $es_networkhost           = '0.0.0.0',
     $es_unicast               = '"127.0.0.1"',
-		$es_httpport							=	'9200-9300',
+    $es_httpport              = '9200-9300',
     $es_minimum_masternodes   = '1',
     $es_recoverafter          = '1',
     $es_java                  = '500m',
