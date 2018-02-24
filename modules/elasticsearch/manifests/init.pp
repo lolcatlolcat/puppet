070a99712b24fcf2245fc48257e61c6ac5b6cae9
Matthew Morrison <matthewmorrison89@gmail.com>
2018-02-24 17:55:56 -0500

    Changed variable names to es since it's shorter.

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -1,18 +1,19 @@
 class elasticsearch (
-    $elasticsearch_clustername                = $hostname,
-    $elasticsearch_nodename                   = $fqdn,
-    $elasticsearch_noderack                   = 'r1',
-    $elasticsearch_memorylock                 = 'true',
-    $elasticsearch_networkhost                = '0.0.0.0',
-    $elasticsearch_unicast                    = '"site-elk", "site-elk-node"',
-    $elasticsearch_minimum_masternodes        = '1',
-    $elasticsearch_recoverafter               = '1',
-    $elasticsearch_java                       = '500m',
-    $es_mem_lock                              = 'unlimited',
-    $es_max_files                             = '65535',
-    $es_master_node                           = undef,
-    $es_data_node                             = undef,
-    $es_ingest_node                           = undef,
+    $es_clustername						= $hostname,
+    $es_nodename              = $fqdn,
+    $es_noderack              = 'r1',
+    $es_memorylock            = 'true',
+    $es_networkhost           = '0.0.0.0',
+    $es_unicast               = '"127.0.0.1"',
+		$es_httpport							=	'9200-9300'.
+    $es_minimum_masternodes   = '1',
+    $es_recoverafter          = '1',
+    $es_java                  = '500m',
+    $es_mem_lock              = 'unlimited',
+    $es_max_files             = '65535',
+    $es_master_node           = true,
+    $es_data_node             = true,
+    $es_ingest_node           = true,
 ){
     class { 'elasticsearch::install' : } ->
     class { 'elasticsearch::configs' : } ->
