5a868a1a6a2dd67fef13999684497fe1993c2a07
matthewmorrison89@gmail.com <morrison@simspace.com>
2018-03-20 10:38:14 +0200

    Playing around with the 'best' way to copy a bunch of template files

--- modules/logstash/manifests/configs.pp
+++ modules/logstash/manifests/configs.pp
@@ -4,5 +4,13 @@ class logstash::configs inherits ::logstash {
     "/etc/logstash/jvm.options" : content => template( "logstash/jvm.options.erb" );
     "/etc/logstash/startup.options" : content => template( "logstash/startup.options.erb" );
     "/etc/logstash/log4j2.properties" : content => template( "logstash/log4j2.properties.erb")
+  } ->
+  file { '/etc/logstash/conf.d':
+    path    =>  '/etc/logstash/conf.d',
+    ensure  =>  directory,
+    source  =>  "puppet:///modules/logstash/conf.d",
+    recurse =>  true,
+    owner   =>  'logstash',
+    group   =>  'logstash',
   }
 }
